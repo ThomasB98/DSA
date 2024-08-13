130c955dfa1426f17eb6684c525979bc20252f04
ThomasB98 <bansodthomas@gmail.com>
2024-08-13 12:54:54 +0530

    [Thomas]UPDATED: LongestConsecutiveSequence Solution

--- Arrays/LongestConsecutiveSequence.cs
+++ Arrays/LongestConsecutiveSequence.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Arrays_Basic
 {
@@ -10,7 +8,32 @@ namespace Arrays_Basic
     {
         static void Main(string[] args)
         {
-            int[] nums = [2, 20, 4, 10, 3, 4, 5];
+            int[] nums = { 2, 20, 4, 10, 3, 4, 5 };
+            HashSet<int> set = nums.ToHashSet();
+            int MaxLength = 0;
+
+            Console.WriteLine(string.Join(", ", set));
+
+            foreach (int num in nums)
+            {
+       
+                if (!set.Contains(num - 1))
+                {
+                    int currentNum = num;
+                    int currentLength = 1;
+
+                    while (set.Contains(currentNum + 1))
+                    {
+                        currentNum += 1;
+                        currentLength += 1;
+                    }
+
+
+                    MaxLength = Math.Max(currentLength, MaxLength);
+                }
+            }
+
+            Console.WriteLine("The length of the longest consecutive sequence is: " + MaxLength);
         }
     }
 }
